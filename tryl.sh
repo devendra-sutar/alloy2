493230c77b3f1a1e739040f17f5a6021046bc43e
devendra-sutar <167191447+devendra-sutar@users.noreply.github.com>
2024-11-18 16:24:38 +0530

    Update tryl.sh

--- tryl.sh
+++ tryl.sh
@@ -71,3 +71,18 @@ echo "Verifying Alloy service status..."
 sudo systemctl status alloy.service
 
 echo "Script completed successfully."
+
+# Step 11: Send POST request with JSON data to the specified endpoint
+echo "Sending data to the server..."
+
+curl -X POST http://10.0.34.138:8000/api/v1/create-agent/ \
+-H "Content-Type: application/json" \
+-d '{
+    "host_name": "AAdmin-new",
+    "ip_port": "192.162.1.12:8080",
+    "keycloak_id": "a00e1a35-1550-4215-930a-1468298be901",
+    "agent_name": "Gitlab",
+    "status": "Active"
+}'
+
+echo "Data sent successfully."
