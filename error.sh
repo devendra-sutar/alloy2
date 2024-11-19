751757832c12d470dbf21b5b2f95d5a256f37584
devendra-sutar <167191447+devendra-sutar@users.noreply.github.com>
2024-11-19 10:49:33 +0530

    Update error.sh

--- error.sh
+++ error.sh
@@ -1,9 +1,1 @@
-curl -X POST http://10.0.34.138:8000/api/v1/create-agent/ \
-    -H "Content-Type: application/json" \
-    -d '{
-        "host_name": "AAdmin-new1",
-        "ip_port": "199.162.1.777:8080",
-        "keycloak_id": "a00e1a35-1550-4215-930a-1468298be901",
-        "agent_name": "Linux123",
-        "status": "Active"
-    }'
+{"error": "UNIQUE constraint failed: userAuth_app_agent.ip_port"}
