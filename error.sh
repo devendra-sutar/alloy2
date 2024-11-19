f8190aa4a28183f35243e302844beb56308d68cf
devendra-sutar <167191447+devendra-sutar@users.noreply.github.com>
2024-11-19 11:20:56 +0530

    Update error.sh

--- error.sh
+++ error.sh
@@ -1,19 +1,9 @@
-enerating unique ip_port...
-Sending POST request to create new agent...
-./linux_clone2.sh: line 141: host_name:: command not found
-./linux_clone2.sh: line 142: ip_port:: command not found
-./linux_clone2.sh: line 143: keycloak_id:: command not found
-./linux_clone2.sh: line 144: agent_name:: command not found
-./linux_clone2.sh: line 145: status:: command not found
-./linux_clone2.sh: line 151: })
-
-# Capture the full response body (for debugging)
-full_response=$(curl -s -X POST http://10.0.34.138:8000/api/v1/create-agent/ \
-    -H "Content-Type: application/json" \
-    -d {: No such file or directory
-./linux_clone2.sh: line 152: host_name:: command not found
-./linux_clone2.sh: line 153: ip_port:: command not found
-./linux_clone2.sh: line 154: keycloak_id:: command not found
-./linux_clone2.sh: line 155: agent_name:: command not found
-./linux_clone2.sh: line 156: status:: command not found
-./linux_clone2.sh: line 157: unexpected EOF while looking for m
+curl -X POST http://10.0.34.138:8000/api/v1/create-agent/ \
+-H "Content-Type: application/json" \
+-d '{
+    "host_name": "AAdmin-new",
+    "ip_port": "192.162.1.12:8080",
+    "keycloak_id": "a00e1a35-1550-4215-930a-1468298be901",
+    "agent_name": "Gitlab",
+    "status": "Active"
+}'
