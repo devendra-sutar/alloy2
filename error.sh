430b338dae3c294b058f9694774adb63302c2d46
devendra-sutar <167191447+devendra-sutar@users.noreply.github.com>
2024-11-19 11:05:05 +0530

    Update error.sh

--- error.sh
+++ error.sh
@@ -1,1 +1,19 @@
-{"error": "UNIQUE constraint failed: userAuth_app_agent.ip_port"}
+enerating unique ip_port...
+Sending POST request to create new agent...
+./linux_clone2.sh: line 141: host_name:: command not found
+./linux_clone2.sh: line 142: ip_port:: command not found
+./linux_clone2.sh: line 143: keycloak_id:: command not found
+./linux_clone2.sh: line 144: agent_name:: command not found
+./linux_clone2.sh: line 145: status:: command not found
+./linux_clone2.sh: line 151: })
+
+# Capture the full response body (for debugging)
+full_response=$(curl -s -X POST http://10.0.34.138:8000/api/v1/create-agent/ \
+    -H "Content-Type: application/json" \
+    -d {: No such file or directory
+./linux_clone2.sh: line 152: host_name:: command not found
+./linux_clone2.sh: line 153: ip_port:: command not found
+./linux_clone2.sh: line 154: keycloak_id:: command not found
+./linux_clone2.sh: line 155: agent_name:: command not found
+./linux_clone2.sh: line 156: status:: command not found
+./linux_clone2.sh: line 157: unexpected EOF while looking for m
