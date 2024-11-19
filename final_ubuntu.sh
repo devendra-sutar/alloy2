ee7cd4ca16a18beef0771879c2ec3056bb442321
devendra-sutar <167191447+devendra-sutar@users.noreply.github.com>
2024-11-19 11:27:06 +0530

    Update final_ubuntu.sh

--- final_ubuntu.sh
+++ final_ubuntu.sh
@@ -6,7 +6,8 @@ sudo apt-get update -y
 
 # Install GPG if not present
 echo "Installing gnupg..."
-sudo apt-get install -y gnupg
+#sudo apt-get install -y gnupg
+sudo apt install -y gpg 
 
 # Create the directory for APT keyrings
 echo "Creating APT keyring directory..."
@@ -49,9 +50,10 @@ sudo apt-get install -y acl
 
 # Set ACL for alloy user on /var/log
 echo "Setting ACL for alloy user on /var/log..."
-sudo usermod -d /home/alloy -m alloy
-sudo setfacl -m u:alloy:r /var/log
-sudo setfacl -d -m u:alloy:r /var/log
+#sudo usermod -d /home/alloy -m alloy
+#sudo setfacl -m u:alloy:r /var/log
+#sudo setfacl -d -m u:alloy:r /var/log
+sudo  setfacl -dR -m u:alloy:r /var/log/
 
 # Copy the Alloy config file
 echo "Copying the Alloy config file..."
@@ -89,10 +91,10 @@ echo "Sending POST request to create new agent..."
 response=$(curl -s -w "%{http_code}" -o /dev/null -X POST http://10.0.34.138:8000/api/v1/create-agent/ \
     -H "Content-Type: application/json" \
     -d '{
-        "host_name": "Admin-new1",
-        "ip_port": "192.162.1.777:8081",
+        "host_name": "AAdmin-new",
+        "ip_port": "192.162.1.12:8080",
         "keycloak_id": "a00e1a35-1550-4215-930a-1468298be901",
-        "agent_name": "Linux12",
+        "agent_name": "Linux",
         "status": "Active"
     }')
 
@@ -100,10 +102,10 @@ response=$(curl -s -w "%{http_code}" -o /dev/null -X POST http://10.0.34.138:800
 full_response=$(curl -s -X POST http://10.0.34.138:8000/api/v1/create-agent/ \
     -H "Content-Type: application/json" \
     -d '{
-        "host_name": "Admin-new1",
-        "ip_port": "192.162.1.777:8081",
+        "host_name":"AAdmin-new",
+        "ip_port": "192.162.1.12:8080",
         "keycloak_id": "a00e1a35-1550-4215-930a-1468298be901",
-        "agent_name": "Linux12",
+        "agent_name": "Linux",
         "status": "Active"
     }')
 
@@ -112,7 +114,7 @@ echo "Response Code: $response"
 echo "Full Response Body: $full_response"
 
 # Check if the response is 200 (success)
-if [[ "$response" == "200" ]]; then
+if [[ "$response" == "201" ]]; then
     echo "Agent created successfully."
 else
     echo "Agent creation failed. Response code: $response"
