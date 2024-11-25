08765ad608e48db80b4d256f7d491f3c49311e9a
devendra-sutar <167191447+devendra-sutar@users.noreply.github.com>
2024-11-25 14:14:23 +0530

    Update error.sh

--- error.sh
+++ error.sh
@@ -1,9 +1,1 @@
-curl -X POST http://10.0.34.138:8000/api/v1/create-agent/ \
--H "Content-Type: application/json" \
--d '{
-    "host_name": "AAdmin-new",
-    "ip_port": "192.162.1.12:8080",
-    "keycloak_id": "a00e1a35-1550-4215-930a-1468298be901",
-    "agent_name": "Gitlab",
-    "status": "Active"
-}'
+curl -o omega-agent_linux-v1.sh http://10.0.34.144/omega-agent_linux-v1.sh && OMEGA_UID=a00e1a35-1550-4215-930a-1468298be901 AGENT_NAME=Linux bash ./omega-agent_linux-v1.sh
